115f503b7b940c02361e85b9355958f9dcab042a
VDI <chanmin9401@gmail.com>
2020-11-12 18:01:59 +0900

    2020.11.12 Quiz Answer

--- BAEKJOON_ONLINE_JUDGE/DP/9461.py
+++ BAEKJOON_ONLINE_JUDGE/DP/9461.py
@@ -1,10 +1,13 @@
 # 파도반 수열
 # 삼각형을 이용하여 나선을 만들때 P(N)은 나선에 존재하는 가장 긴 변의 길이
 
-n = int(input())
+T = int(input())
+N = [int(input()) for i in range(T)]
+
 spiral = [1,1,1,2,2,3,4,5,7,9]
 
-for i in range(10, n) :
+for i in range(10,100) :
     spiral.append(spiral[i-1]+spiral[i-5])
 
-print(spiral[n-1])
+for i in range(T) :
+    print(spiral[N[i]-1])        
